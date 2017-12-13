09dea2111e01677b3b3c0c11edf55c8f8c1f54d8
goalgate <522508134@qq.com>
2017-12-13 11:56:00 +0800

    小修小補

--- app/src/main/java/com/hninstrument/AppInit.java
+++ app/src/main/java/com/hninstrument/AppInit.java
@@ -2,32 +2,12 @@ package com.hninstrument;
 
 import android.app.Application;
 import android.content.Context;
-
-import com.blankj.utilcode.util.ActivityUtils;
-import com.blankj.utilcode.util.SPUtils;
-import com.blankj.utilcode.util.ToastUtils;
 import com.blankj.utilcode.util.Utils;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import com.hninstrument.Tools.DaoMaster;
 import com.hninstrument.Tools.DaoSession;
 import com.squareup.leakcanary.LeakCanary;
-
 import org.greenrobot.greendao.database.Database;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import cbdi.log.Lg;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by zbsz on 2017/11/25.
@@ -45,7 +25,6 @@ public class AppInit extends Application {
     public static Context getContext() {
         return getInstance().getApplicationContext();
     }
-
 
     @Override
     public void onCreate() {
